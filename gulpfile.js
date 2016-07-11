4103d23744e23d8d42a5556a6a20fd6471266c84
Scott <dev@misonline.biz>
2016-07-11 16:56:06 -0400

    Major version update to es/es7 app, build tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,48 @@
+
 var config=require('./config.json');
 var Tasks=require('elliptical-gulp');
 var gulp=require('gulp');
-
+var ScaffoldTasks=require('elliptical-scaffold');
 
 var tasks=new Tasks(config);
+var scaffoldTasks=new ScaffoldTasks(config);
 
+// app tasks -----------------------------------------------------------------------------------------------------------
+
+gulp.task('init',function(){
+    build();
+});
 
 gulp.task('default',function(){
     tasks.default();
-});
-
-gulp.task('start-live-server',function(){
-    tasks.startLiveServer();
 });
 
 gulp.task('start-server',function(){
     tasks.startServer();
 });
 
-gulp.task('start-live',function(){
-    tasks.startLive();
-});
-
 gulp.task('start',function(){
     tasks.start();
-});
-
-gulp.task('start-live-app',function(){
-    tasks.startLiveApp();
 });
 
 gulp.task('start-app',function(){
     tasks.startApp();
 });
 
-gulp.task('start-live-sass',function(){
-    tasks.startLiveSass();
-});
-
-gulp.task('start-sass',function(){
-    tasks.startSass();
-});
-
-gulp.task('start-live-app-no-sass',function(){
-    tasks.startLiveAppNoSass();
-});
-
 gulp.task('start-app-no-sass',function(){
     tasks.startAppNoSass();
-});
-
-gulp.task('start-live-scripts',function(){
-    tasks.startLiveScripts();
-});
-
-gulp.task('start-scripts',function(){
-    tasks.startScripts();
 });
 
 gulp.task('sass-compile', function () {
     tasks.sassCompile();
 });
 
+gulp.task('sass-compile-min', function () {
+    tasks.sassCompileMin();
+});
+
 gulp.task('sass-watch', function () {
     tasks.sassWatch();
-});
-
-gulp.task('scripts-watch', function () {
-    tasks.scriptsWatch();
-});
-
-gulp.task('scripts-build', function () {
-    tasks.scriptsBuild();
 });
 
 gulp.task('app-watch', function () {
@@ -82,18 +53,195 @@ gulp.task('app-build', function () {
     tasks.appBuild();
 });
 
-gulp.task('vulcanize', function () {
-    tasks.vulcanize();
-});
-
-gulp.task('app-build-imports', function () {
-    tasks.appBuildImports();
-});
-
-gulp.task('app-watch-imports', function () {
-    tasks.appWatchImports();
+gulp.task('app-imports', function () {
+    tasks.appImports();
 });
 
 gulp.task('watch', function () {
     tasks.watch();
 });
+
+gulp.task('app-clean', function () {
+    tasks.appClean();
+});
+
+gulp.task('vulcanize', function () {
+    tasks.vulcanize();
+});
+
+gulp.task('vulcanize-min', function () {
+    tasks.vulcanizeMin();
+});
+
+
+
+///scaffold tasks ------------------------------------------------------------------------------------------------------
+///app -----------------------------------------------------------------------------------------------------------------
+gulp.task('app-crud-controller', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var className=argv.class;
+    var icon=argv.icon;
+    if(icon===undefined) icon='edit';
+    if(className===undefined){
+        console.log("Error: Class required");
+        return;
+    }
+    var params={
+        class:className,
+        icon:icon
+    };
+    scaffoldTasks.appCreateCrudController(config,params);
+});
+
+gulp.task('app-empty-controller', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var name=argv.name;
+    if(name===undefined){
+        console.log("Error: Controller name required");
+        return;
+    }
+    var params={
+        name:name
+    };
+    scaffoldTasks.appCreateEmptyController(config,params);
+});
+
+gulp.task('app-empty-view', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var name=argv.name;
+    var folder=argv.folder;
+    if(name===undefined){
+        console.log("Error: view name required");
+        return;
+    }
+    if(folder===undefined){
+        console.log("Error: view folder required");
+        return;
+    }
+    var params={
+        name:name,
+        folder:folder
+    };
+    scaffoldTasks.appCreateEmptyView(config,params);
+});
+
+gulp.task('app-list-view', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var name=argv.name;
+    var folder=argv.folder;
+    var className=argv.class;
+    var icon=argv.icon;
+    if(name===undefined){
+        console.log("Error: view name required");
+        return;
+    }
+    if(folder===undefined){
+        console.log("Error: view folder required");
+        return;
+    }
+    if(className===undefined){
+        console.log("Error: Class required");
+        return;
+    }
+    if(icon===undefined) icon='edit';
+    var params={
+        name:name,
+        folder:folder,
+        class:className,
+        icon:icon
+    };
+    scaffoldTasks.appCreateListView(config,params);
+});
+
+gulp.task('app-detail-view', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var name=argv.name;
+    var folder=argv.folder;
+    var className=argv.class;
+    var icon=argv.icon;
+    if(name===undefined){
+        console.log("Error: view name required");
+        return;
+    }
+    if(folder===undefined){
+        console.log("Error: view folder required");
+        return;
+    }
+    if(className===undefined){
+        console.log("Error: Class required");
+        return;
+    }
+    if(icon===undefined) icon='edit';
+    var params={
+        name:name,
+        folder:folder,
+        class:className,
+        icon:icon
+    };
+    scaffoldTasks.appCreateDetailView(config,params);
+});
+
+gulp.task('app-binding', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var name=argv.name;
+    if(name===undefined){
+        console.log("Error:  name required");
+        return;
+    }
+    var params={
+        name:name
+    };
+    scaffoldTasks.appCreateBinding(config,params);
+});
+
+gulp.task('app-service', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var className=argv.class;
+    if(className===undefined){
+        console.log("Error: class required");
+        return;
+    }
+    var params={
+        class:className
+    };
+    scaffoldTasks.appCreateService(config,params);
+});
+
+gulp.task('app-provider', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var className=argv.class;
+    if(className===undefined){
+        console.log("Error: class required");
+        return;
+    }
+    var params={
+        class:className
+    };
+    scaffoldTasks.appCreateProvider(config,params);
+});
+
+gulp.task('app-scaffold', function () {
+    scaffoldTasks.appScaffold(config);
+});
+
+/// web component-------------------------------------------------------------------------------------------------------
+gulp.task('web-component', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var tag=argv.tag;
+    var dir=argv.d;
+    if(tag===undefined){
+        console.log("Error: tag required");
+        return;
+    }
+    if(dir===undefined){
+        console.log("Error: directory location required");
+        return;
+    }
+    var params={
+        tag:tag,
+        dir:dir
+    };
+    scaffoldTasks.webCreateComponent(config,params);
+});
+
+
